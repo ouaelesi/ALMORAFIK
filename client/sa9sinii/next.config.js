a43816f6107498a967d9bc1a45f45a0335d622cd
ouaelesi <ouael2019esi@gmail.com>
2021-10-31 02:26:18 -0700

    add: handle change of the create question form

--- client/sa9sinii/next.config.js
+++ client/sa9sinii/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     PORT:4000 , 
-    MONGO_URI:"mongodb+srv://ouaelSahbi:esi123aze123aze@cluster0.swcln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"  
+    MONGO_URI:"mongodb+srv://ouaelSahbi:esi123aze123aze@cluster0.swcln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true"  
 }
 }
--- client/sa9sinii/partials/QuestionBody.js
+++ client/sa9sinii/partials/QuestionBody.js
@@ -1,42 +1,77 @@
-import React, { Component } from "react";
-import {FormGroup,Input} from 'reactstrap'
+import React, { useEffect, useState } from "react";
+import { FormGroup, Input, Spinner } from "reactstrap";
 
-class QuestionBody extends Component {
-  render() {
-    return (
-      <div>
-        <div className="ASKYOURQUES">Ask Your Question</div>
-        <div className="QuestionBody">
-          <FormGroup>
-            <p className="QuestionTitle">Title</p>
-            <p className="QuestionEXP">
-              Be specific and imagine you’re asking a question to another person
-            </p>
-            <Input type="text" name="QuestionTitle" id="QuestionTitle"></Input>
+const QuestionBody = () => {
+  // State
+  const [form, setForm] = useState({ question: "" });
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  //
+  useEffect(() => {
+  }, [isSubmiting]);
+  //handle chnages & submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmiting(true);
+  };
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <div>
+      {isSubmiting ? (
+        <Spinner />
+      ) : (
+        <div>
+          <div className="ASKYOURQUES">Ask Your Question</div>
+          <div className="QuestionBody">
+            <form onSubmit={handleSubmit}>
+              <p className="QuestionTitle">Title</p>
+              <p className="QuestionEXP">
+                Be specific and imagine you’re asking a question to another
+                person
+              </p>
 
-            <p className="QuestionTitle">Body</p>
-            <p className="QuestionEXP">
-              Include all the information someone would need to answer your
-              question
-            </p>
-            <textarea
-              className="form-control Question_text"
-              name="QuestionTitle"
-              id="QuestionTitle "
-              rows="6"
-            ></textarea>
+              <Input
+                type="text"
+                name="QuestionTitle"
+                id="QuestionTitle"
+                onChange={handleChange}
+              ></Input>
 
-            <p className="QuestionTitle">Field</p>
-            <p className="QuestionEXP">
-              Add to describe what your question is about
-            </p>
-            <Input type="text" name="QuestionTitle" id="QuestionTitle"></Input>
-          </FormGroup>
+              <p className="QuestionTitle">Body</p>
+              <p className="QuestionEXP">
+                Include all the information someone would need to answer your
+                question
+              </p>
+              <textarea
+                className="form-control Question_text"
+                name="question"
+                id="question"
+                rows="6"
+                onChange={handleChange}
+              ></textarea>
+
+              <p className="QuestionTitle">Field</p>
+              <p className="QuestionEXP">
+                Add to describe what your question is about
+              </p>
+              <Input
+                type="text"
+                name="QuestionTitle"
+                id="QuestionTitle"
+              ></Input>
+              <button className="btn review_btn" type="submit">
+                Add Question
+              </button>
+            </form>
+          </div>
         </div>
-        <button className="btn review_btn">Review Your Question</button>
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
 export default QuestionBody;
